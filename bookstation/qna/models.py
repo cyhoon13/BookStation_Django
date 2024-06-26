2c3c415b316a4792c9aca62b6b16a0507ff2e1a5
younghoon cho <cyhoon13@gmail.com>
2024-06-26 17:37:35 +0900

    06/26 수정

--- bookstation/qna/models.py
+++ bookstation/qna/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class qna(models.Model):
-    qna_id = models.IntegerField(primary_key=True) # 문의 번호
+    qna_id = models.AutoField(primary_key=True) # 문의 번호
     member_id = models.CharField(max_length=30) # 회원 아이디
-    qna_date = models.DateTimeField()  # 작성 날짜
+    qna_date = models.DateField(default=timezone.now)  # 작성 날짜
     qna_type = models.IntegerField() # 문의 유형
     qna_title = models.CharField(max_length=100) # 문의 제목
     qna_content = models.CharField(max_length=1000) # 문의 내용
-    qna_state = models.CharField(max_length=40) # 처리상태
+    qna_state = models.CharField(max_length=40, default="답변중") # 처리상태
--- bookstation/qna/views.py
+++ bookstation/qna/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import qna
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator #페이지 처리해주는 클래스
+from django.utils import timezone
 # Create your views here.
 
 #문의내역
 def question(request):
-    # return render(request, "qna/question.html")
-    # user = request.user
-    # if not user.is_authenticated:
-        # 로그인되지 않은 사용자는 로그인 페이지로 리디렉션
-        # return redirect('login')
+    # 세션에서 로그인된 사용자 ID 가져오기
+    member_id = request.session.get('login_id')
+    if not member_id:
+        # 세션에 로그인 ID가 없으면 로그인 페이지로 리디렉션
+        return redirect('login')
 
-    #question_list = Question.objects.all()
-    qna_list = qna.objects.filter(member_id="member1").order_by("qna_date")
+    # 로그인된 사용자의 문의 내역을 필터링하여 가져오기
+    qna_list = qna.objects.filter(member_id=member_id).order_by("qna_date")
     paginator = Paginator(qna_list, 10)  # 페이지당 10개 항목 표시
 
     page_number = request.GET.get('page')
@@ -23,7 +24,34 @@ def question(request):
 
 #문의 작성하기
 def qnaWrite(request):
-    return render(request,"qna/qnaWrite.html")
+    if request.method != 'POST':  # GET 방식
+        member_id = request.session.get('login_id')
+        if not member_id:
+            return redirect('login')
+        return render(request, "qna/qnaWrite.html", {'member_id': member_id})
+    else:  # POST 방식
+        member_id = request.session.get('login_id')
+        if not member_id:
+            return redirect('login')
+
+        # 폼 데이터에서 값 가져오기
+        qna_type = request.POST.get('qna_type')
+        qna_title = request.POST.get('qna_title')
+        qna_content = request.POST.get('qna_content')
+
+        # 디버깅 출력
+        print(f"member_id: {member_id}, qna_type: {qna_type}, qna_title: {qna_title}, qna_content: {qna_content}")
+
+        # Qna 객체 생성 및 저장
+        q = qna(
+            member_id=member_id,
+            qna_type=qna_type,
+            qna_title=qna_title,
+            qna_content=qna_content
+        )
+        q.save()  # insert, update -> save(), delete -> delete()
+
+        return redirect('question')  # 'question' URL 이름으로 리디렉션
 
 #문의 세부내용
 def qnaDetail(request):
