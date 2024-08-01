729d4e655029d2ec9ac949fab3a4aa488a073625
cyhoon13 <113090656+cyhoon13@users.noreply.github.com>
2024-08-01 22:13:36 +0900

    Add files via upload

--- bookstation/qna/utils.py
+++ bookstation/qna/utils.py
@@ -1,7 +1,6 @@
 import os
 import tensorflow as tf
 from transformers import BertTokenizer, TFBertForSequenceClassification
-import logging
 
 def load_tokenizer():
     # 경로 수정
@@ -17,24 +16,20 @@ def load_model():
     model_path = os.path.join(os.path.dirname(__file__), '..', 'models', 'bert_model')
 
     # 모델 로드
-    model = TFBertForSequenceClassification.from_pretrained(model_path)
+    model = TFBertForSequenceClassification.from_pretrained(model_path)  # 내부적으로 소프트맥스 함수를 사용하여 로짓 값을 확률 값으로 변환
 
     return model
-
-logger = logging.getLogger(__name__)
 
 def check_profanity(text):
     tokenizer = load_tokenizer()
     model = load_model()
     
-    encodings = tokenizer([text], truncation=True, padding=True, max_length=128)
+    encodings = tokenizer([text], truncation=True, padding=True, max_length=40)
     dataset = tf.data.Dataset.from_tensor_slices(dict(encodings)).batch(1)
-    
-    logger.debug(f"Encoded text for profanity check: {encodings}")
 
     predictions = model.predict(dataset)
+
+    #예측된 로짓 값을 통해 가장 높은 확률의 라벨을 선택
     predicted_label = tf.argmax(predictions.logits, axis=1).numpy()[0]
-    
-    logger.debug(f"Prediction result: {predicted_label}")
 
     return predicted_label == 1  # 1이면 욕설, 0이면 비욕설
